14b656f10cd03afe9dddfafc19ead9d321908c49
Santhosh <santhoshkumarr1096@gmail.com>
2019-08-30 20:43:22 +0530

    using pointer for Error

--- oops.go
+++ oops.go
@@ -46,7 +46,7 @@ type stack struct {
 }
 
 //JSON converts Error into json format to for structured logging
-func (err Error) JSON() map[string]interface{} {
+func (err *Error) JSON() map[string]interface{} {
 	json := make(map[string]interface{})
 	json["error"] = err.error
 	json["info"] = err.info
@@ -55,31 +55,31 @@ func (err Error) JSON() map[string]interface{} {
 }
 
 //TraceFormat registers Errorformat function to print trace in a format
-func (err Error) TraceFormat(f ErrorTraceFormat) Error {
+func (err *Error) TraceFormat(f ErrorTraceFormat) *Error {
 	err.traceFormat = f
 	return err
 }
 
 //ErrorFormat registers Errorformat function to print error in a format
-func (err Error) ErrorFormat(f ErrorHeaderFormat) Error {
+func (err *Error) ErrorFormat(f ErrorHeaderFormat) *Error {
 	err.errorFormat = f
 	return err
 }
 func (s stack) format(f string) string {
 	return fmt.Sprintf(f, s.FuncName, s.Line, s.File)
 }
-func (err Error) Error() string {
+func (err *Error) Error() string {
 	return err.errorWithSkip(err.skip)
 }
 
 //Info lets you add more information about the error
-func (err Error) Info(value string) Error {
+func (err *Error) Info(value string) *Error {
 	err.info = value
 	return err
 }
 
 //Skip skips n functions from bottom of the stack
-func (err Error) Skip(n int) Error {
+func (err *Error) Skip(n int) *Error {
 	if n > len(err.stackTrace) {
 		n = len(err.stackTrace)
 	}
@@ -88,18 +88,18 @@ func (err Error) Skip(n int) Error {
 }
 
 //Line sets line no where error occured
-func (err Error) Line(value int) Error {
+func (err *Error) Line(value int) *Error {
 	err.stackTrace[0].Line = value
 	return err
 }
 
 //Func sets function name where error occured
-func (err Error) Func(value string) Error {
+func (err *Error) Func(value string) *Error {
 	err.stackTrace[0].FuncName = value
 	return err
 }
 
-func (err Error) errorWithSkip(skip int) string {
+func (err *Error) errorWithSkip(skip int) string {
 	if skip > len(err.stackTrace) {
 		skip = len(err.stackTrace)
 	}
@@ -113,20 +113,19 @@ func (err Error) errorWithSkip(skip int) string {
 }
 
 //Origin prints where error got originated, not the trace
-func (err Error) Origin() string {
+func (err *Error) Origin() string {
 	return err.errorWithSkip(len(err.stackTrace) - 1)
 }
 
 //T add error with more information like stacktrace
 //corresponding to the where function got called
-func T(err error) Error {
-	var err1 Error
+func T(err error) *Error {
 	if err == nil {
-		return err1
+		return nil
 	}
 	switch err.(type) {
-	case Error:
-		return err.(Error)
+	case *Error:
+		return err.(*Error)
 	}
 	pc := make([]uintptr, 10)
 	runtime.Callers(2, pc)
@@ -143,7 +142,7 @@ func T(err error) Error {
 			FuncName: f.Function,
 		})
 	}
-	return Error{error: err, stackTrace: st, traceFormat: dTraceFormat, errorFormat: dErrorFormat}
+	return &Error{error: err, stackTrace: st, traceFormat: dTraceFormat, errorFormat: dErrorFormat}
 }
 
 func formatFileName(fileName string) string {
